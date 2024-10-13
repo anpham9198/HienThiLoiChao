95ed2934279d163b5c55fb918f03c3725af642fa
anpham9198 <anpham9198@gmail.com>
2024-10-13 15:21:26 +0700

    Add project files.

---
+++ HienThiLoiChao/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        string yourName = Console.ReadLine();
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
